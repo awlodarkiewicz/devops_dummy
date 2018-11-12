09a743a05d9d351c1efe3de2cbd0614e10aad675
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-11-12 18:40:28 +0100

    dummy files

--- scripts/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ${INSTALLATION_PATH}
-
-mv dummy_file ${INSTALLATION_PATH}/
---
+++ scripts/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+mkdir -p ${INSTALLATION_PATH}
+
+mv dummy_file ${INSTALLATION_PATH}/
